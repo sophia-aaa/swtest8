2c98e42e59105779f82adf6e4cc4b6f70d2826b4
sophia-aaa <fragrance0627@gmail.com>
2021-04-30 01:04:45 +0200

    first commit

---
+++ src/test/java/com/example/swtest8/Swtest8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swtest8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swtest8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
